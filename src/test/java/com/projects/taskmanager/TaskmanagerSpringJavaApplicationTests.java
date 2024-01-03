55c4b0853c047f967c63517513c5bd8d31080ee3
Palanivel <palanivel.developer@gmail.com>
2024-01-03 18:17:16 +0530

    Initialize spring java

---
+++ src/test/java/com/projects/taskmanager/TaskmanagerSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerSpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
